19473b32986cbbc7e0fa776bb240dad742989671
hamadialvin <hamadialvin@yahoo.com>
2020-10-14 14:12:13 +0300

    Changes to POM dependancies

--- api/src/main/java/org/openmrs/module/immunization/api/impl/ImmunizationServiceImpl.java
+++ api/src/main/java/org/openmrs/module/immunization/api/impl/ImmunizationServiceImpl.java
@@ -1,11 +1,11 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+ * the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * OpenMRS is also distributed under the terms of the Healthcare Disclaimer located at
+ * http://openmrs.org/license.
  *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
+ * <p>Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS graphic logo is a
+ * trademark of OpenMRS Inc.
  */
 package org.openmrs.module.immunization.api.impl;
 
@@ -17,36 +17,32 @@ import org.openmrs.module.immunization.api.ImmunizationService;
 import org.openmrs.module.immunization.api.dao.ImmunizationDao;
 
 public class ImmunizationServiceImpl extends BaseOpenmrsService implements ImmunizationService {
-	
-	ImmunizationDao dao;
-	
-	UserService userService;
-	
-	/**
-	 * Injected in moduleApplicationContext.xml
-	 */
-	public void setDao(ImmunizationDao dao) {
-		this.dao = dao;
-	}
-	
-	/**
-	 * Injected in moduleApplicationContext.xml
-	 */
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
-	
-	@Override
-	public Item getItemByUuid(String uuid) throws APIException {
-		return dao.getItemByUuid(uuid);
-	}
-	
-	@Override
-	public Item saveItem(Item item) throws APIException {
-		if (item.getOwner() == null) {
-			item.setOwner(userService.getUser(1));
-		}
-		
-		return dao.saveItem(item);
-	}
+
+  ImmunizationDao dao;
+
+  UserService userService;
+
+  /** Injected in moduleApplicationContext.xml */
+  public void setDao(ImmunizationDao dao) {
+    this.dao = dao;
+  }
+
+  /** Injected in moduleApplicationContext.xml */
+  public void setUserService(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Override
+  public Item getItemByUuid(String uuid) throws APIException {
+    return dao.getItemByUuid(uuid);
+  }
+
+  @Override
+  public Item saveItem(Item item) throws APIException {
+    if (item.getOwner() == null) {
+      item.setOwner(userService.getUser(1));
+    }
+
+    return dao.saveItem(item);
+  }
 }
