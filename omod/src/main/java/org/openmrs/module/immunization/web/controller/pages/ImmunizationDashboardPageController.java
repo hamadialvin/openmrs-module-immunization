0435bf5bafc7e3ea47a27c4a15ebd27644d9d2f8
hamadialvin <hamadialvin@yahoo.com>
2020-10-26 20:18:28 +0300

    Added immunizationDashboad page

--- omod/src/main/java/org/openmrs/module/immunization/web/controller/pages/ImmunizationDashboardPageController.java
+++ omod/src/main/java/org/openmrs/module/immunization/web/controller/pages/ImmunizationDashboardPageController.java
@@ -1,4 +1,19 @@
 package org.openmrs.module.immunization.web.controller.pages;
 
+import org.openmrs.Patient;
+import org.openmrs.module.kenyaemr.EmrWebConstants;
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.page.PageModel;
+
 public class ImmunizationDashboardPageController {
+  public String controller(UiUtils ui, PageModel model) {
+
+    Patient patient = (Patient) model.getAttribute(EmrWebConstants.MODEL_ATTR_CURRENT_PATIENT);
+
+    if (patient != null) {
+      return "redirect:immunizationSearchSeen";
+    } else {
+      return null;
+    }
+  }
 }
